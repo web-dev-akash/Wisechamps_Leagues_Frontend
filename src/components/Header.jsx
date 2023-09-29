2beb28af7af7e3b64f3a33a09692bd1c03c16649
web-dev-akash <akumar415129.ak@gmail.com>
2023-09-29 13:00:51 +0530

    Info button with UI updated / pending help icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,9 +15,11 @@ import {
   Tabs,
 } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
+import { HiOutlineInformationCircle } from "react-icons/hi";
 export const Header = () => {
   const { leagueCategory } = useSelector((state) => state);
   const [headerClass, setHeaderClass] = useState("");
+  const [toolTip, setTooltip] = useState(false);
   const league = {
     Stone: 0,
     Iron: 1,
@@ -25,6 +27,18 @@ export const Header = () => {
     Silver: 3,
     Gold: 4,
     Diamond: 5,
+  };
+
+  const toggleTooltip = () => {
+    setTooltip(!toolTip);
+  };
+
+  const toggleTooltipFalse = () => {
+    setTooltip(false);
+  };
+
+  const toggleTooltipTrue = () => {
+    setTooltip(true);
   };
 
   useEffect(() => {
@@ -36,93 +50,280 @@ export const Header = () => {
   return (
     <div>
       <header>
-        <h4>Weekly Leaderboard</h4>
+        <div className="info-icon">
+          <HiOutlineInformationCircle
+            onClick={toggleTooltip}
+            onMouseEnter={toggleTooltipTrue}
+            onMouseLeave={toggleTooltipFalse}
+          />
+        </div>
+        <h3>Weekly Leaderboard</h3>
         <Spacer height={2} />
         <hr />
       </header>
       <section className={headerClass}>
         <Tabs isFitted variant="enclosed" defaultIndex={league[leagueCategory]}>
-          <TabList>
+          <TabList borderColor={"blackAlpha.500"}>
             <Tab
               bgColor={"white"}
-              padding={"10px 0px"}
+              padding={league[leagueCategory] === 0 ? "5px 20px" : "0"}
               isDisabled={league[leagueCategory] !== 0}
               className={league[leagueCategory] === 0 ? "active" : ""}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                position: "relative",
+              }}
             >
+              <p
+                style={{
+                  opacity: 0,
+                  transition: "all 0.3s linear",
+                  display:
+                    league[leagueCategory] !== 0 && toolTip ? "block" : "none",
+                }}
+                className={
+                  league[leagueCategory] !== 0 && toolTip ? "show" : "hide"
+                }
+              >
+                Stone
+              </p>
               <img width={"35px"} src={stone} alt="league-logo" />
+              <p
+                style={{
+                  transition: "all 0.3s linear",
+                  position: "relative",
+                  fontSize: "11px",
+                  top: "0.5rem",
+                  width: "150%",
+                  opacity: "0",
+                  color: "black",
+                  display: league[leagueCategory] === 0 ? "block" : "none",
+                }}
+              >
+                Stone League
+              </p>
             </Tab>
             <Tab
               bgColor={"white"}
-              padding={"10px 0px"}
+              padding={league[leagueCategory] === 1 ? "5px 20px" : "0"}
               isDisabled={league[leagueCategory] !== 1}
               className={league[leagueCategory] === 1 ? "active" : ""}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                position: "relative",
+              }}
             >
+              <p
+                style={{
+                  opacity: 0,
+                  transition: "all 0.3s linear",
+                  display:
+                    league[leagueCategory] !== 1 && toolTip ? "block" : "none",
+                }}
+                className={
+                  league[leagueCategory] !== 1 && toolTip ? "show" : "hide"
+                }
+              >
+                Iron
+              </p>
               <img width={"35px"} src={iron} alt="league-logo" />
+              <p
+                style={{
+                  transition: "all 0.3s linear",
+                  position: "relative",
+                  fontSize: "11px",
+                  top: "0.5rem",
+                  width: "150%",
+                  opacity: "0",
+                  color: "black",
+                  display: league[leagueCategory] === 1 ? "block" : "none",
+                }}
+              >
+                Iron League
+              </p>
             </Tab>
             <Tab
               bgColor={"white"}
+              padding={league[leagueCategory] === 2 ? "5px 20px" : "0"}
               isDisabled={league[leagueCategory] !== 2}
               className={league[leagueCategory] === 2 ? "active" : ""}
-              padding={"10px 0px"}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                position: "relative",
+              }}
             >
+              <p
+                style={{
+                  opacity: 0,
+                  transition: "all 0.3s linear",
+                  display:
+                    league[leagueCategory] !== 2 && toolTip ? "block" : "none",
+                }}
+                className={
+                  league[leagueCategory] !== 2 && toolTip ? "show" : "hide"
+                }
+              >
+                Bronze
+              </p>
               <img width={"35px"} src={bronze} alt="league-logo" />
+              <p
+                style={{
+                  transition: "all 0.3s linear",
+                  position: "relative",
+                  fontSize: "11px",
+                  top: "0.5rem",
+                  width: "150%",
+                  opacity: "0",
+                  color: "black",
+                  display: league[leagueCategory] === 2 ? "block" : "none",
+                }}
+              >
+                Bronze League
+              </p>
             </Tab>
             <Tab
               bgColor={"white"}
+              padding={league[leagueCategory] === 3 ? "5px 20px" : "0"}
               isDisabled={league[leagueCategory] !== 3}
               className={league[leagueCategory] === 3 ? "active" : ""}
-              padding={"10px 0px"}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                position: "relative",
+              }}
             >
+              <p
+                style={{
+                  opacity: 0,
+                  transition: "all 0.3s linear",
+                  display:
+                    league[leagueCategory] !== 3 && toolTip ? "block" : "none",
+                }}
+                className={
+                  league[leagueCategory] !== 3 && toolTip ? "show" : "hide"
+                }
+              >
+                Silver
+              </p>
               <img width={"35px"} src={silver} alt="league-logo" />
+              <p
+                style={{
+                  transition: "all 0.3s linear",
+                  position: "relative",
+                  fontSize: "11px",
+                  top: "0.5rem",
+                  width: "150%",
+                  opacity: "0",
+                  color: "black",
+                  display: league[leagueCategory] === 3 ? "block" : "none",
+                }}
+              >
+                Silver League
+              </p>
             </Tab>
             <Tab
               bgColor={"white"}
+              padding={league[leagueCategory] === 4 ? "5px 20px" : "0"}
               isDisabled={league[leagueCategory] !== 4}
               className={league[leagueCategory] === 4 ? "active" : ""}
-              padding={"10px 0px"}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                position: "relative",
+              }}
             >
+              <p
+                style={{
+                  opacity: 0,
+                  transition: "all 0.3s linear",
+                  display:
+                    league[leagueCategory] !== 4 && toolTip ? "block" : "none",
+                }}
+                className={
+                  league[leagueCategory] !== 4 && toolTip ? "show" : "hide"
+                }
+              >
+                Gold
+              </p>
               <img width={"35px"} src={gold} alt="league-logo" />
+              <p
+                style={{
+                  transition: "all 0.3s linear",
+                  position: "relative",
+                  fontSize: "11px",
+                  top: "0.5rem",
+                  width: "150%",
+                  opacity: "0",
+                  color: "black",
+                  display: league[leagueCategory] === 4 ? "block" : "none",
+                }}
+              >
+                Gold League
+              </p>
             </Tab>
             <Tab
               bgColor={"white"}
-              padding={"10px 0px"}
+              padding={league[leagueCategory] === 5 ? "5px 20px" : "0"}
               isDisabled={league[leagueCategory] !== 5}
               className={league[leagueCategory] === 5 ? "active" : ""}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                position: "relative",
+              }}
             >
+              <p
+                style={{
+                  opacity: 0,
+                  transition: "all 0.3s linear",
+                  display:
+                    league[leagueCategory] !== 5 && toolTip ? "block" : "none",
+                }}
+                className={
+                  league[leagueCategory] !== 5 && toolTip ? "show" : "hide"
+                }
+              >
+                Diamond
+              </p>
               <img width={"35px"} src={diamond} alt="league-logo" />
+              <p
+                style={{
+                  transition: "all 0.3s linear",
+                  position: "relative",
+                  fontSize: "11px",
+                  top: "0.5rem",
+                  width: "150%",
+                  opacity: "0",
+                  color: "black",
+                  display: league[leagueCategory] === 5 ? "block" : "none",
+                }}
+              >
+                Diamond League
+              </p>
             </Tab>
           </TabList>
           <TabPanels>
-            <TabPanel className="leagueName">
-              <div>
-                <p>Stone League</p>
-              </div>
-            </TabPanel>
-            <TabPanel className="leagueName">
-              <div>
-                <p>Iron League</p>
-              </div>
-            </TabPanel>
-            <TabPanel className="leagueName">
-              <div>
-                <p>Bronze League</p>
-              </div>
-            </TabPanel>
-            <TabPanel className="leagueName">
-              <div>
-                <p>Silver League</p>
-              </div>
-            </TabPanel>
-            <TabPanel className="leagueName">
-              <div>
-                <p>Gold League</p>
-              </div>
-            </TabPanel>
-            <TabPanel className="leagueName">
-              <div>
-                <p>Diamond League</p>
-              </div>
-            </TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
           </TabPanels>
         </Tabs>
       </section>
